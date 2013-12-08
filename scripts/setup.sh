e206a6e488e77055a63fb35b3501c96f25756802
Viktor Hansson <viktor.hansson@me.com>
2013-12-08 21:40:10 +0100

    Removed: 	script/flags.rb 	sublime-workspace
    Changed:
    	Added license headers to all my files

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,17 @@
 #! bin/bash
-
+#        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+#                    Version 2, December 2004 
+#
+# Copyright (C) 2013 Viktor Hansson <viktor.hansson@me.com>
+#
+# Everyone is permitted to copy and distribute verbatim or modified 
+# copies of this license document, and changing it is allowed as long 
+# as the name is changed. 
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
 mkdir bin
 mkdir bin/debug
 mkdir bin/release
